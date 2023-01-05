a4b893b9674cadcef33ec0e0d45f57c573de6f66
Yalovechik2012 <yalovechik2012@gmail.com>
2023-01-05 17:36:25 +0200

    commit

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header></header>
+      <header>hello</header>
       <main className="">
         <p className="text-3xl font-bold underline">maidddn</p>
       </main>
